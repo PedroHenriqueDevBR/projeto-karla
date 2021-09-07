31ebc8aaca51f52ba24ab03d2367057204de81de
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-09-07 09:57:22 -0300

    feat: response response with events

--- projeto_da_carla/api_v1/serializers.py
+++ projeto_da_carla/api_v1/serializers.py
@@ -2,7 +2,13 @@ from django.db.models import fields
 from rest_framework.serializers import ModelSerializer
 from core.models import *
 
+class TrackSerializer(ModelSerializer):
+    class Meta:
+        model = EventResponse
+        fields = ['id', 'response_date', 'guest_name', 'confirm']
+
 class EventSerializer(ModelSerializer):
+    responses = TrackSerializer(many=True, read_only=True)
     class Meta:
         model = Event
         fields = [
@@ -16,6 +22,7 @@ class EventSerializer(ModelSerializer):
             'expiration_date',
             'password',
             'person',
+            'responses',
         ]
 
 class ResponseSerializer(ModelSerializer):
--- projeto_da_carla/projeto_da_carla/settings.py
+++ projeto_da_carla/projeto_da_carla/settings.py
@@ -50,8 +50,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=31),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
 }
 
 
